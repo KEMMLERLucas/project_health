a7bb09f7bcbe2259757b428383806c4b1767aca9
hiekashi <93048928+hiekashi@users.noreply.github.com>
2024-04-05 21:31:25 +0200

    Update PatientPreview.jsx

--- src/components/PatientPreview.jsx
+++ src/components/PatientPreview.jsx
@@ -4,8 +4,6 @@ import Banner from "./Banner.jsx";
 
 function PatientPreview({patient}){
     return (<div className="PatientList">
-        <Banner/>
-        <p>Mes patients</p>
         <PatientList patient={patient}/>
         </div>
     )
