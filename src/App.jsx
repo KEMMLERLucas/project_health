67743a8d042edcf6382e6f86f1027c2610df9a95
KEMMLER Lucas <kemmler2u@etu.univ-lorraine.fr>
2024-04-05 17:48:25 +0200

    First version of form

--- src/App.jsx
+++ src/App.jsx
@@ -4,90 +4,93 @@ import {useEffect, useState} from "react";
 import SuiviPatient from "./components/SuiviPatient.jsx";
 
 function App() {
-  const [isLoading,setLoading] =useState(false)
-  const[isError,setError] =useState(false)
-  const[patients, setPatients] = useState([])
-  const[activities, setActivities] = useState([])
-  const[steps, setSteps] = useState([])
-  
-
-  useEffect(() => {
-    async function loadPatient(){
-      const api="https://health.shrp.dev/items/people"
-      try{
-        setLoading(true)
-        setError(false)
-        const response = await axios.get(api)
-        const data  = await response.data.data
-
-        setLoading(false)
-        setError(false)
-
-        setPatients(data);
-      }catch (error){
-        console.error(error)
-        setLoading(false)
-        setError(true)
-      }
-    }
-    loadPatient()
-  }, []);
-
-  useEffect(() => {
-    async function loadActivities(){
-    const api="https://health.shrp.dev/items/physicalActivities"
-    try{
-        setLoading(true)
-        setError(false)
-        const response = await axios.get(api)
-        const data  = await response.data.data
-
-        setLoading(false)
-        setError(false)
-
-        setActivities(data);
-    }catch (error){
-        console.error(error)
-        setLoading(false)
-        setError(true)
-    }
-    }
-    loadActivities()
-}, []);
-
-useEffect(() => {
-  async function loadSteps(){
-    const api="http://localhost:8000/steps"
-    try{
-      setLoading(true)
-      setError(false)
-      const response = await axios.get(api)
-      const data  = await response.data.data
-
-      setLoading(false)
-      setError(false)
-
-      setSteps(data);
-    }catch (error){
-      console.error(error)
-      setLoading(false)
-      setError(true)
-    }
-  }
-  loadSteps()
-}, []);
+    const [isLoading, setLoading] = useState(false)
+    const [isError, setError] = useState(false)
+    const [patients, setPatients] = useState([])
+    const [activities, setActivities] = useState([])
+    const [steps, setSteps] = useState([])
 
 
-  return (
-      <div className="App">
-        {isLoading && <p>Chargement....</p>}
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        {isError && <p> Une erreur s'est produite</p>}
-        {patients.map((patient)=> (
-            < SuiviPatient key={patient.id} patient={patient} activities={activities} steps = {steps}/> ))
+    useEffect(() => {
+        async function loadPatient() {
+            const api = "https://health.shrp.dev/items/people"
+            try {
+                setLoading(true)
+                setError(false)
+                const response = await axios.get(api)
+                const data = await response.data.data
+
+                setLoading(false)
+                setError(false)
+
+                setPatients(data);
+            } catch (error) {
+                console.error(error)
+                setLoading(false)
+                setError(true)
+            }
         }
-      </div>
-  )
+
+        loadPatient()
+    }, []);
+
+    useEffect(() => {
+        async function loadActivities() {
+            const api = "https://health.shrp.dev/items/physicalActivities"
+            try {
+                setLoading(true)
+                setError(false)
+                const response = await axios.get(api)
+                const data = await response.data.data
+
+                setLoading(false)
+                setError(false)
+
+                setActivities(data);
+            } catch (error) {
+                console.error(error)
+                setLoading(false)
+                setError(true)
+            }
+        }
+
+        loadActivities()
+    }, []);
+
+    useEffect(() => {
+        async function loadSteps() {
+            const api = "http://localhost:8000/steps"
+            try {
+                setLoading(true)
+                setError(false)
+                const response = await axios.get(api)
+                const data = await response.data.data
+
+                setLoading(false)
+                setError(false)
+
+                setSteps(data);
+            } catch (error) {
+                console.error(error)
+                setLoading(false)
+                setError(true)
+            }
+        }
+
+        loadSteps()
+    }, []);
+
+
+    return (
+        <div className="App">
+            {isLoading && <p>Chargement....</p>}
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            {isError && <p> Une erreur s'est produite</p>}
+            {patients.map((patient) => (
+                < SuiviPatient key={patient.id} patient={patient} activities={activities} steps={steps}/>))
+            }
+        </div>
+    )
 }
 
 export default App;
--- src/components/Connexion.jsx
+++ src/components/Connexion.jsx
@@ -1,31 +1,63 @@
 import './auth.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {useState} from "react";
+import {useForm} from "react-hook-form"
 
 function Connexion() {
-    return (
-      <div className="block_log">
+    const {
+        register, handleSubmit
+    } = useForm()
+    const onSubmit = (data) => console.log(data)
+    return (<div className="block_log">
+            <div className="formulaire">
+                <h2>Connexion</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="champ">
+                        <label htmlFor="email">Adresse e-mail</label>
+                        <input type="text" id="email"
+                               placeholder="Saisir votre adresse e-mail" {...register("email", {required: true, maxLength:90})}/>
+                    </div>
+                    <div className="champ">
+                        <label htmlFor="mdp" id="mdp">Mot de passe</label>
+                        <input type="password" id="mdp"
+                               placeholder="Saisir votre mot de passe"/> {...register("password")}/>
+                    </div>
+
+                    <p id="forget_mdp">Mot de passe oublié ?</p>
+
+                    <span className="button">
+                        <button className="primary_button">Se connecter</button>
+                    </span>
+                    <p>Pas de compte ? <Link id="register" to="/inscription">Je m'inscris !</Link></p>
+                </form>
+            </div>
+        </div>
+
+    )
+    return (<div className="block_log">
         <div className="formulaire">
-          <h2>Connexion</h2>
+            <h2>Connexion</h2>
 
-          <div className="champ">
-          <label for="email">Adresse e-mail</label>
-          <input type="text" id="email" placeholder="Saisir votre adresse e-mail"/>
-          </div>
+            <div className="champ">
+                <label for="email">Adresse e-mail</label>
+                <input type="text" id="email"
+                       placeholder="Saisir votre adresse e-mail"/>
+            </div>
 
-          <div className="champ">
-          <label for="mdp" id="mdp">Mot de passe</label>
-          <input type="password" id="mdp" placeholder="Saisir votre mot de passe"/>
-          </div>
+            <div className="champ">
+                <label for="mdp" id="mdp">Mot de passe</label>
+                <input type="password" id="mdp"
+                       placeholder="Saisir votre mot de passe"/>
+            </div>
 
-          <p id="forget_mdp">Mot de passe oublié ?</p>
+            <p id="forget_mdp">Mot de passe oublié ?</p>
 
-          <span className="button">
+            <span className="button">
           <button className="primary_button">Se connecter</button>
           </span>
-          <p>Pas de compte ? <Link id="register" to="/inscription">Je m'inscris !</Link></p>
+            <p>Pas de compte ? <Link id="register" to="/inscription">Je m'inscris !</Link></p>
         </div>
-      </div>
-    )
-  }
-  
-  export default Connexion
+    </div>)
+}
+
+export default Connexion
