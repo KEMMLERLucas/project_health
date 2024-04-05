8be6c9e91a49a8ba763676e78a2d372388e7cc15
KEMMLER Lucas <kemmler2u@etu.univ-lorraine.fr>
2024-04-05 16:22:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import axios from "axios"
 import './App.css'
 import {useEffect, useState} from "react";
-import SuiviPatient from "./components/SuiviPatient.jsx";
-import PatientList from "./components/PatientList.jsx";
 import PatientPreview from "./components/PatientPreview.jsx";
 
 function App() {
