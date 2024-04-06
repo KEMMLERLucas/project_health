98c319cf9debcc2ec464077704fa227a57acc110
Justine200 <heroltjustine@gmail.com>
2024-04-06 17:08:26 +0200

    Affichage des données dans le placeholder des inputs
    Appel à l'api pour obtenir un seul patient, pour tester (ça sera sûrement à modifier plus tard)

--- src/components/InformationPatient.jsx
+++ src/components/InformationPatient.jsx
@@ -1,14 +1,15 @@
 import "./SuiviPatient.css";
+import axios from "axios"
 import { Link } from 'react-router-dom'
 import Title from "./Title.jsx";
 import PatientName from "./PatientName";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Onglets from "./Onglets.jsx";
 import StatsActu from "./StatsActu.jsx";
 import Graphes from "./Graphes.jsx";
 
 
-function InformationPatient({patient}){
+function InformationPatient(){
 
     const [activeTab, setActiveTab] = useState("Personnelles");
 
@@ -19,6 +20,24 @@ function InformationPatient({patient}){
             return "Femme"
         }
     }
+    const[patient, setPatient] = useState([])
+    
+
+    useEffect(() => {
+        async function loadPatient(){
+        const api="https://health.shrp.dev/items/people/00657896-d299-48a7-8a41-aae1c2b4f606"
+        try{
+            const response = await axios.get(api)
+            const data  = await response.data.data
+
+            setPatient(data);
+            console.log(patient);
+        }catch (error){
+            console.error(error)
+        }
+        }
+        loadPatient()
+    }, []);
 
     return (
         <div>
@@ -35,27 +54,27 @@ function InformationPatient({patient}){
                     <div className="formulaire">
                         <div className="champ">
                             <label htmlFor="prénom">Prénom</label>
-                            <input type="text" id="prénom" placeholder="{patient.firstname}"/>
+                            <input type="text" id="prénom" placeholder={patient.firstname}/>
                         </div>
 
                         <div className="champ">
                             <label htmlFor="nom">Nom</label>
-                            <input type="text" id="nom" placeholder="{patient.lastname}"/>
+                            <input type="text" id="nom" placeholder={patient.lastname}/>
                         </div>
 
                         <div className="champ">
                             <label htmlFor="sexe">Sexe</label>
-                            <input type="text" id="Sexe" placeholder="{getSexeText(patient.sex)}"/>
+                            <input type="text" id="Sexe" placeholder={getSexeText(patient.sex)}/>
                         </div>
 
                         <div className="champ">
-                            <label htmlFor="dateDeNaissance">Date De Naissance</label>
-                            <input type="text" id="dateDeNaissance" placeholder="{patient.birthday}"/>
+                            <label htmlFor="dateDeNaissance">Année De Naissance</label>
+                            <input type="text" id="dateDeNaissance" placeholder={patient.birthyear}/>
                         </div>
 
                         <div className="champ">
                             <label htmlFor="taille">Taille</label>
-                            <input type="text" id="taille" placeholder="{patient.firstname}"/>
+                            <input type="text" id="taille" placeholder={patient.height + " cm"}/>
                         </div>
 
                         <span className="button"><button className="primary_button">Mettre à jour les informations</button></span>
