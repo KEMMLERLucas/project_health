a9f7bd8995e5d0fe99707a5f9f0256480d5746c5
hiekashi <93048928+hiekashi@users.noreply.github.com>
2024-04-08 13:10:54 +0200

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,14 +2,17 @@ import "./SuiviPatient.css";
 import { FaCaretLeft } from "react-icons/fa";
 import { FaCog } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-function Title({name}){
+function Title({name, history}){
+    
+    const handleGoBack = () => {
+        history.goBack();
+    };
 
     return (
     <div className="title">
-        <Link to="/">
-            <FaCaretLeft className="icone" id="fleche"/>
-        </Link>
+        <FaCaretLeft className="icone" id="fleche" onClick={handleGoBack}/>
         <span>{name}</span>
         <Link to="/info">
             <FaCog className="icone" id="rouage"/>
