d113ac11d15d5044042cf8c3b867e6911fc8a9c4
Justine200 <heroltjustine@gmail.com>
2024-04-15 15:32:17 +0200

    Affichage des entrainement de la semaine dans la partie "Derniers entrainements"

--- src/components/DerniersEntrainements.jsx
+++ src/components/DerniersEntrainements.jsx
@@ -6,7 +6,6 @@ import {useEffect, useState} from "react";
 
 
 function DerniersEntrainements ({ patient }) {
-    var listActivities = patient.physicalActivities
     function clicked() {
         console.log("Cliqué : " + patient.firstname + " ; activité clicked");
     }
@@ -30,6 +29,28 @@ function DerniersEntrainements ({ patient }) {
         loadActivities()
     }, []);
 
+    function getDate(activity){
+        let today = dateWeek(new Date());
+        let activityDate;
+        let lastWeek = false;
+
+        activityDate = dateWeek(new Date(activity.date));
+        if(activityDate === today){
+            lastWeek = true;
+        }
+        return(lastWeek);
+            /*console.log(today);*/
+            /*console.log(new Date());*/
+    }
+
+    function dateWeek(a) {
+        var d = a ? new Date(a) : new Date();
+        d.setHours(0,0,0,0);
+        d.setDate(d.getDate() + 3 - (d.getDay() + 6) % 7);
+        var w = new Date(d.getFullYear(), 0, 4);
+        return ('0' + (1 + Math.round(((d.getTime() - w.getTime()) / 86400000 - 3 + (w.getDay() + 6) % 7) / 7))).slice(-2);
+    }
+
     /*
     * name, duration and calories should be changed
     * string here to give an example just
@@ -40,7 +61,7 @@ function DerniersEntrainements ({ patient }) {
             <div className="title" id="titleEntrainement"> Ses derniers entraînements</div>
             <div className="DerniersEntrainements" onClick={clicked}>
                 {activities.map((activity)=> (
-                    listActivities.includes(activity.id) && <ChampEntrainement key={activity.id} name={activity.type} duration={activity.duration+" minutes"} calories={activity.consumedCalories+" calories"}/>))
+                    getDate(activity) && <ChampEntrainement key={activity.id} name={activity.type} duration={activity.duration+" minutes"} calories={activity.consumedCalories+" calories"}/>))
                 }
             </div>
         </div>
