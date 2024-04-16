bce7b75d32c0137c63cea2fb139f9494ec1b0128
hiekashi <93048928+hiekashi@users.noreply.github.com>
2024-04-16 15:07:26 +0200

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,12 @@ import "./banner.css";
 import React, { useState } from 'react';
 import { FaRegBell } from "react-icons/fa";
 import { FaCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Banner(){
     return (<div className="banner">
         <div className="infos_banner">
+        <Link to="/infos">
         <div class="test">
             <div id="photo"></div>
             <div id="text">
@@ -14,7 +16,7 @@ function Banner(){
         <FaCircle className="icone" id="circle"/> Connecté
         </div>
         </div>
-        </div>
+        </div></Link>
         </div>
         <FaRegBell className="icone" id="bell"/>        
     </div>)
